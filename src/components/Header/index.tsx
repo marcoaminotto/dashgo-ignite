1753dd9363f904850163d2e18dce27463da3605a
Marco Echevestre <marcoechevestre@Marcos-MBP.localdomain>
2021-10-25 13:01:54 +0200

    Make header responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,35 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 
-import { Logo } from './Logo';
-import { NotificationNav } from './NotificationNav';
-import { Profile } from './Profile';
-import { SearchBox } from './SearchBox';
+import { Logo } from "./Logo";
+import { NotificationNav } from "./NotificationNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
 
 export function Header() {
-  return (
-    <Flex
-      as="header"
-      w="100%"
-      maxWidth={1480}
-      h="20"
-      mx="auto"
-      mt="4"
-      px="6"
-      align="center"
-    >
-      <Logo />
-      <SearchBox />
-      <Flex align="center" ml="auto">
-        <NotificationNav />
-        <Profile />
-      </Flex>
-    </Flex>
-  );
+	const isWideVersion = useBreakpointValue({
+		base: false,
+		lg: true,
+	});
+
+	return (
+		<Flex
+			as="header"
+			w="100%"
+			maxWidth={1480}
+			h="20"
+			mx="auto"
+			mt="4"
+			px="6"
+			align="center"
+		>
+			<Logo />
+
+			{isWideVersion && <SearchBox />}
+
+			<Flex align="center" ml="auto">
+				<NotificationNav />
+				<Profile showProfileDate={isWideVersion} />
+			</Flex>
+		</Flex>
+	);
 }
